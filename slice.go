57cbe69ade4d1d0de64b2f48169421e201732510
Zakaria Wahyu <zakarianur6@gmail.com>
2022-08-22 20:22:48 +0700

    Slice

--- slice.go
+++ slice.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	people := make([]string, 10)
+	people := make([]string, 0, 10)
 	people = append(people, "zaka", "fajar", "zayan", "rizki", "hendar", "billy", "maulana", "agus", "ramadan", "utomo")
 
 	for _, val := range people {
